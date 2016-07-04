b5eb2b5baae99890036214f1f01ef8c1b5383bf4
Иван <jouk0v2@mail.ru>
2016-07-04 15:45:05 +0400

    BOT

--- SuperApple.java
+++ SuperApple.java
@@ -8,16 +8,16 @@ public class SuperApple extends Apple {
     public int x2, y2, x3, y3;
 
     public SuperApple(int[][] matrix) {
-        x = super.random.nextInt(60);
-        y = super.random.nextInt(60);
+        x = super.random.nextInt(58) + 1;
+        y = super.random.nextInt(58) + 1;
         x2 = x + 1;
         y2 = y;
         x3 = x;
         y3 = y + 1;
 
-        while ( ( matrix[y][x] == 1 ) || (matrix[y2][x2] == 1) || ( matrix[y3][x3] == 1 ) || (y == 60) || (x == 60)) {
-            x = random.nextInt(60);
-            y = random.nextInt(60);
+        while ( ( matrix[y][x] == 1 ) || (matrix[y2][x2] == 1) || ( matrix[y3][x3] == 1 ) || (y == 59) || (x == 59)) {
+            x = random.nextInt(58) + 1;
+            y = random.nextInt(58) + 1;
         }
         paintApple(matrix);
     }
